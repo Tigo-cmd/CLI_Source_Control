9fab34b0d5fbcbd0983a1b22a086a1e9d670b9c6
Tigo-cmd <ja16162040@gmail.com>
2024-05-13 21:15:33 +0100

    updated file to create files from anywhere

--- sc.py
+++ sc.py
@@ -7,7 +7,6 @@ Originally By Nwali Ugonna Emmanuel (Emmanuel Tigo)
 ###################################################################################################################
 """
 
-
 from sub_func.repo import Git
 from sub_func.create_py_classes import create_parent_class
 from sub_func.create_func_files import source_code_create, file_create
@@ -18,7 +17,9 @@ import os
 main_entr = Git()
 
 
-def source_control():
+#  main entry of the code, runs all functionalities
+def main():
+    """main function calls all necessary functions for functionality"""
     """Handles the command line interface interaction utilizing the Argparse module"""
     source = ArgumentParser(description="Command Line Source Control", prog="SC")
     source.add_argument('-t', '--touch', metavar="filename", nargs='+', help="creates files")
@@ -47,38 +48,31 @@ def source_control():
     if args.touch:
         for i in args.touch:  # loops through the arguments passed and creates the files
             file_create(i)  # function that's creates the file
-
-    # uses the -a switch to add files to git
-    if args.add:
-        for i in args.add:
-            main_entr.git_add(i)  # calls the git add function
-
-    # handles the git commit got tracked changes
-    if args.commit:  # checks if the -c switch is passed to the command line
-        if args.add:  # checks if -a optional argument is passed
-            if args.commit:
-                msg = input("commit message: ")  # asks user for commit message and commit changes
-                if msg == "":
-                    main_entr.git_commit()  # send default commit message if user passes no message whe prompted
-                else:
-                    main_entr.git_commit(msg)
-            else:
-                main_entr.git_commit(str(args.commit))
-        else:
-            print("run SC -a ('filenames') to add files before commit")
-
-    # pushes changes to git
-    if args.push:
-        """calls the git push command to push changes"""
-        main_entr.git_push()
-
-
-#  main entry of the code, runs all functionalities
-def main():
-    """main function calls all necessary functions for functionality"""
     if main_entr.is_git():  # returns True if git is installed in the machine else False see
         if main_entr.is_git_repo():  # checks if the current repo is a git repository and returns True
-            source_control()  # calls then source control function implemented above
+            # uses the -a switch to add files to git
+            if args.add:
+                for i in args.add:
+                    main_entr.git_add(i)  # calls the git add function
+
+            # handles the git commit got tracked changes
+            if args.commit:  # checks if the -c switch is passed to the command line
+                if args.add:  # checks if -a optional argument is passed
+                    if args.commit:
+                        msg = input("commit message: ")  # asks user for commit message and commit changes
+                        if msg == "":
+                            main_entr.git_commit()  # send default commit message if user passes no message whe prompted
+                        else:
+                            main_entr.git_commit(msg)
+                    else:
+                        main_entr.git_commit(str(args.commit))
+                else:
+                    print("run SC -a ('filenames') to add files before commit")
+
+            # pushes changes to git
+            if args.push:
+                """calls the git push command to push changes"""
+                main_entr.git_push()
             print(main_entr)  # prints "program ran successfully"
     else:
         print("Error Coming soon")
