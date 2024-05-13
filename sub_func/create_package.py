c594c516421916097e921eced63c1a8adcc15704
Tigo-cmd <ja16162040@gmail.com>
2024-05-13 21:44:16 +0100

    New Commit

--- sub_func/create_package.py
+++ sub_func/create_package.py
@@ -1,16 +1,21 @@
-"""this modue handles package creation for python"""
+"""this module handles package creation for python"""
 
 import os
 
-def create_package(rootdir="", subdir=""):
+
+def create_package(dir=""):
     """implements the package creation
     Args:
-        rootdir: this is the parent directory or package name
-        subdir: sub folders and directory for package
+        dir: this is the parent directory or package name
     """
-    if rootdir != "":
+    if dir != "":
         # creates the directory
-        os.mkdir(rootdir, 0o764)
-        #adds the __init__ file...
+        os.mkdir(dir, 0o764)
+        # changes dir to dir and creates the init.py file
+        os.chdir(dir)
+        # adds the __init__ file...
         init_file = "__init__.py"
-        with open(init_file, 'w', encoding='utf-8':
+        with open(init_file, 'w', encoding='utf-8') as f:
+            pass
+    else:
+        pass
