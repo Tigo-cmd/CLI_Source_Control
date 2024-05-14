0b9990b79988b43a0a05ac6a3a6f3fafa78cd7b8
Tigo-cmd <ja16162040@gmail.com>
2024-05-14 16:42:51 +0100

    fixed regular expression

--- sub_func/create_py_classes.py
+++ sub_func/create_py_classes.py
@@ -12,11 +12,15 @@ from sub_func.create_func_files import is_exists
 
 
 def create_parent_class(filename="", instance_prototype=""):
-    pattern = r'[\w/]+\.py'
-    match = re.search(pattern, filename)
-    if match:
-        class_file = match.group(0)
     """handles class creation and constructor(instance) creation it utilizes regular expression"""
-    content = ["#!/usr/bin/python3\n", '"""module documentation"""\n\n',f"class {class_file[:-3]}()\n"
-                                                                        f"\t"]
-    is_exists(filename, content)
+    if filename[-3:] == '.py':
+        pattern = r'[\w/]+\.py'
+        match = re.search(pattern, filename)
+        class_file = match.group(0)
+        content = ["#!/usr/bin/python3\n", '"""module documentation"""\n\n',f"class {class_file[:-3]}:\n"
+                                                                            f"\t"]
+        is_exists(filename, content)
+    else:
+        with open(filename, 'w', encoding='utf-8') as file:
+            pass
+        print("created an empty file")
