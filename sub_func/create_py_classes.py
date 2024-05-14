6f0ef70de155443535e72d4a00905104805ef90d
Tigo-cmd <ja16162040@gmail.com>
2024-05-14 16:22:17 +0100

    fixed bugs

--- sub_func/create_py_classes.py
+++ sub_func/create_py_classes.py
@@ -7,30 +7,16 @@ Originally By Nwali Ugonna Emmanuel (Emmanuel Tigo)
 ###################################################################################################################
     """
 import os
+import re
+from sub_func.create_func_files import is_exists
 
 
 def create_parent_class(filename="", instance_prototype=""):
-    """handles class creation and constructor(instance) creation"""
-    content = ["#!/usr/bin/python3\n", '"""module documentation"""\n\n',f"class {filename[:-3]}()\n"
-                                                                        f"\t{instance_prototype}"
-                                                                        f"\n\t", "if __name__ == '__main__':"]
-    if os.path.exists(filename):
-        """checks if file exists in the current directory and overwrites when prompted"""
-        overwrite = input(f"{filename} already exists Overwrite?(Y/N): ").lower()
-        if 'y' in overwrite:
-            instance_prototype = input(f"instance_prototype for {filename}: ")
-            with open(filename, 'w', encoding='utf-8') as f:
-                # creates file and writes contents to each line of the file
-                f.writelines(content)
-            os.chmod(filename, 0o764)
-        elif 'n' in overwrite:
-            pass
-        else:
-            print("invalid input")
-            pass
-    else:  # checks if the prototype was given else asks user to input
-        instance_prototype = input(f"prototype for {filename}: ")  # prototype is stored in the variable
-        with open(filename, 'w', encoding='utf-8') as f:
-            # creates file and writes contents to each line of the file
-            f.writelines(content)
-        os.chmod(filename, 0o764)  # set the file permission of the file to executable
+    pattern = r'[\w/]+\.py'
+    match = re.search(pattern, filename)
+    if match:
+        class_file = match.group(0)
+    """handles class creation and constructor(instance) creation it utilizes regular expression"""
+    content = ["#!/usr/bin/python3\n", '"""module documentation"""\n\n',f"class {class_file[:-3]}()\n"
+                                                                        f"\t"]
+    is_exists(filename, content)
